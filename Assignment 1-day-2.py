e2ca85a4e638967fdd44ae412f35b78daa942ca8
Aparna768213 <85796143+Aparna768213@users.noreply.github.com>
2021-06-13 12:20:27 +0530

    Add files via upload

---
+++ Assignment 1-day-2.py
@@ -0,0 +1,52 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("30 days 30 hour challenge")
+30 days 30 hour challenge
+>>> print('30 days 30 hour challenge')
+30 days 30 hour challenge
+>>> #Assigning string to Variables
+>>> hours="thirty"
+>>> print(hours)
+thirty
+>>> #Indexing using String
+>>> Days="Thirty days"
+>>> print(Days[0])
+T
+>>> #print particular character from certain text
+>>> challenge="I will win"
+>>> print(challenge[7:8])
+w
+>>> challenge="I will win"
+>>> print(challenge[7:10])
+win
+>>> #print length of character
+>>> challenge="I will win"
+>>> print(len(challenge))
+10
+>>> #convert into lower character
+>>> challenge="I Will Win"
+>>> print(challenge.lower())
+i will win
+>>> #string concatenation - joining two strings
+>>> a = "30 days"
+>>> b = "30 hours"
+>>> c = a + b
+>>> print(c)
+30 days30 hours
+>>> #adding space during concatenation
+>>> a = "30 days"
+>>> b = "30 hours"
+>>> c = a + " " + b
+>>> print(c)
+30 days 30 hours
+>>> #casefold()-usage
+>>> text ="Thirty days and Thirty hours"
+>>> x = text.casefold()
+>>> print(x)
+thirty days and thirty hours
+>>> x = text.capitalize()
+>>> a ="Learning python is very easy"
+>>> b =a.capitalize()
+>>> print(b)
+Learning python is very easy
+>>> 
